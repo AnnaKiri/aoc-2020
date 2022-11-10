ba84a4c14fdee94efad09ae441ac03e5490d010f
Anna Kirillova <a.kirillova1812@gmail.com>
2022-11-10 18:01:26 +0400

    Part 1 and part 2 of the task1

---
+++ src/Day3/Part1.java
@@ -0,0 +1,50 @@
+package Day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part1 {
+	public static void main(String[] args) {
+		File file = new File(".\\src\\Day3\\data.txt");
+		
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			return;
+		}
+		
+		List<String> map = new ArrayList<>();
+		while (scan.hasNextLine()) {
+			map.add(scan.nextLine());
+		}
+				
+		scan.close();	
+		
+		System.out.print(countTrees(map));
+
+	}
+	
+	public static int countTrees(List<String> map) {
+		int counter = 0;
+		int index = 0;
+		for (int i = 0; i < map.size(); i++) {
+			String str = map.get(i);
+			if (str.charAt(index) == '#') {
+				counter++;
+			}
+			
+			index += 3;
+			if (index > str.length() - 1) {
+				index = index - str.length();
+			}
+		}
+		
+		return counter;
+	
+	}
+}
